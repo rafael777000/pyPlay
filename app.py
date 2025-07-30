7cbbea45dc7d42a9682e59daceeccabab8d0e809
rafael777000 <rafael.leal@dbm.com.br>
2025-07-30 17:23:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-    url = f"https://www.dailymotion.com/{path}"
+    url = f"https://yewtu.be/{path}"
     if request.query_string:
         url += '?' + request.query_string.decode()
 
